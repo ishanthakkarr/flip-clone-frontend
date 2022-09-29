20b3c78aa9b8fb5cf3990cb9a127fbb7d22b327a
Jay Thakkar <jaythakkar2807@gmail.com>
2022-09-29 20:17:31 +0530

    urlconfig changed

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -3,3 +3,11 @@ const baseUrl = window.location.hostname === "localhost" ? "http://localhost:200
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`
 }
+
+// const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = window.location.hostname === "localhost" ? "http://localhost:2000" : "https://flipkart-rest-server-backend.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
